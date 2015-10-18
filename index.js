4354438d65c0fa253970e39b0ad8403933217161
James Halliday <mail@substack.net>
2015-10-18 03:06:08 -0700

    simulator working with a flood algorithm

--- index.js
+++ index.js
@@ -13,28 +13,31 @@ function Router (opts) {
   this.id = opts.id // 8 octets
   if (!this.id) throw new Error('router id required')
   if (this.id.length !== 8) throw new Error('router id must be 8 bytes')
-  this.ifseq = 0
   this.seqno = 0
   this.neighbors = {}
+  this.interfaces = {}
   this.sources = {}
   this.routes = {}
   this.pending = {}
 }
 
-Router.prototype.createStream = function (addr) {
+Router.prototype.createStream = function (ifaceName, addr) {
   var self = this
   var iface = new Iface(addr)
-  var ifdex = self.ifseq++
-  self.neighbors[ifdex] = iface
+  self.interfaces[ifaceName] = iface
   endof(iface, function () {
-    delete self.neighbors[ifdex]
+    delete self.interfaces[ifaceName]
   })
   return iface
 }
 
-Router.prototype.multisend = function (buf) {
-  //this.streams.forEach(function (s) { s.write(buf) })
+Router.prototype.close = function () {
+  var self = this
+  Object.keys(self.interfaces).forEach(function (key) {
+    self.interfaces[key].close()
+  })
 }
 
-Router.prototype.send = function (addr, buf) {
+Router.prototype.lookup = function (addr) {
+  return false
 }
--- sim/network.js
+++ sim/network.js
@@ -1,32 +1,55 @@
 var randombytes = require('randombytes')
-var Introducer = require('../')
+var Router = require('../')
 var through = require('through2')
 var shuf = require('array-shuffle')
+var ip = require('ip-packet')
+var createHash = require('crypto').createHash
 
 var Simulator = require('network-simulator')
 var sim = new Simulator()
 
 var connected = {}, ifaces = {}
-for (var i = 0; i < 50; i++) (function (i) {
+for (var i = 1; i <= 50; i++) (function (i) {
   var node = sim.createNode(i, [ 'eth0', 'eth1', 'eth2', 'eth3', 'eth4' ])
-  ifaces[i] = []
-  var router = new Introducer({ id: randombytes(8) })
+  ifaces[i] = Object.keys(node.ifaces)
+  var router = new Router({ id: randombytes(8) })
+  var seen = {}
   ;[ 'eth0', 'eth1', 'eth2', 'eth3', 'eth4' ].forEach(function (iface) {
     node.on(iface + ':message', function (buf) {
-      console.log('BUF=', buf)
+      var hash = createHash('sha1').update(buf).digest('hex')
+      if (seen[hash]) return
+      seen[hash] = true
+      var packet = ip.decode(buf)
+
+      if (packet.destinationIp.split('.')[3] === String(i)) {
+        console.log(packet)
+      } else if (router.lookup(packet.destinationIp)) {
+        console.log('TODO: intelligently route')
+      } else {
+        Object.keys(node.ifaces).forEach(function (key) {
+          if (iface !== key) node.send(key, buf)
+        })
+      }
     })
   })
-
+  var added = 0
   shuf(Object.keys(sim.nodes)).forEach(function (key) {
+    if (added >= 3) return
     if (key === String(i)) return
     var ckey = [key,i].sort().join(',')
     if (connected[ckey]) return
-    connected[ckey] = true
-    if (!ifaces[key]) ifaces[key] = [ 'eth0', 'eth1', 'eth2', 'eth3', 'eth4' ]
     if (ifaces[key].length === 0) return
     if (ifaces[i].length === 0) return
+    connected[ckey] = true
+    added ++
     node.link(ifaces[i].shift(), sim.nodes[key], ifaces[key].shift())
   })
 })(i)
 
-sim.nodes[0].send('eth0', Buffer('whatever!'))
+sim.nodes[1].send('eth0', ip.encode({
+  version: 4,
+  protocol: 0,
+  sourceIp: '192.168.0.1',
+  destinationIp: '192.168.0.50',
+  data: Buffer('whatever!')
+}))
